842f863baec5ad52be6d28073a7b2c9d04075981
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-04-29 09:20:00 +0545

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,25 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "prettier/prettier": "error",
+            "@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports" }],
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
+            "unused-imports/no-unused-imports": "error",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "unused-imports/no-unused-vars": "off",
+            "no-useless-catch": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
 ];
 
 export default eslintConfig;
