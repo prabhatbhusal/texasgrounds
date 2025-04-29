29646103bcd12f5d7b9e5158e7049a347d91dd84
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-04-29 09:51:34 +0545

    adding files

---
+++ src/lib/constants/data.ts
@@ -0,0 +1,28 @@
+import type {navItems} from '@/types/navbar.types'
+export const NavItems:navItems[] = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 1,
+    title: "Services",
+    url: "/services",
+  },
+  {
+    id: 1,
+    title: "Careers",
+    url: "/careers",
+  },
+  {
+    id: 1,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 1,
+    title: "Blog",
+    url: "/blog",
+  },
+];
---
+++ src/types/navbar.types.ts
@@ -0,0 +1,5 @@
+export type navItems={
+  id:number,
+  title:string,
+  url:string
+}
